9d2e9801d71281db5be363ba477c7d3f264831d3
chiuwa <chiuwa94@gmail.com>
2020-06-01 23:26:27 +0800

    fix title errror

--- resources/views/service.blade.php
+++ resources/views/service.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
-@section('title',setting('site.title') ." | ". __("services_package"))
+@section('title',setting('site.title') ." | ". __('frontend.services_package'))
 
 
 @section('main_page')
