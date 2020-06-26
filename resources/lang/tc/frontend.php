bb3d82873ca4c835a59a198669a63825b805d1b6
chiuwa-company <chiuwa94@gmail.com>
2020-06-26 16:25:47 +0800

    service page v4

--- resources/lang/tc/frontend.php
+++ resources/lang/tc/frontend.php
@@ -19,7 +19,7 @@ return [
 	'home_promotion_content_1'=>'由 開設網店，設計網頁，開發網站，域名，雲端網頁寄存，公司電郵，Google SEO 搜索優化服務，社交平台推廣，一條龍服務，明碼實價一次性收費，無任何隱藏費用。',
 	'home_promotion_title_2'=>'我們',
     'home_promotion_content_2'=>'是一群多年經驗的在職人士組成的Freelance 網站開發設計公司，用較低的價錢就可以獲得專業的服務。設計和開發分別擁有不過的時間表，互相不幹擾。',
-    'intern'=>'煙腸實習Plan',
+    'intern'=>'實習Plan',
     'low_lv_plan'=>'入門Plan',
     'full_lv_plan'=>'標準Plan',
     'free_quota'=>'獲得免費報價',
--- resources/views/service.blade.php
+++ resources/views/service.blade.php
@@ -54,7 +54,8 @@
 						<ul class="service_desc_extra">
 							<li>專業網頁設計</li>
 							<li>支援語言切換</li>
-							<li>有限修改次數</li>
+							<li>後台 CMS 系統</li>
+							<li>包含維護／系統升級</li>								
 							<li>按要求訂制</li>
 							<li>響應式網頁設計</li>
 							<li>中小企公司網頁</li>
@@ -66,7 +67,7 @@
 						<ul class="service_desc_extra">
 							<li>由有經驗員工開發</li>
 							<li>包含設計師費用</li>	
-							<li>包含維護／系統升級</li>						
+												
 						</ul>
 					</div>
 					<div class="chartRadarDiv2" >
@@ -97,32 +98,34 @@
 					
 					<div class="service_desc">
 						<ul class="service_desc_extra">
-							<li>基本網頁設計</li>
+							<li>風格、功能近乎全客製化</li>
+							<li>高度客製化的頁面</li>
+							<li>後台 CMS 系統</li>
 							<li>支援語言切換</li>
-							<li>有限修改次數</li>
-							<li>按要求訂制</li>
+							<li>包含維護／系統升級</li>	
+							<li>按要求完全訂制</li>
 							<li>響應式網頁設計</li>
-							<li>小型企業,個人網站</li>
-							<li>簡介組織或其他簡單用途</li>
+							<li>需要獨特功能</li>
+							<li>要求完全個人化或完全按公司形象設計</li>
 						</ul>
 					</div>
 					<hr>
 					<div class="service_desc_other">
 						<ul class="service_desc_extra">
-							<li>由資深員工帶領</li>
-							<li>應屆畢業生開發</li>
-							<li>低成本</li>						
+							<li>由資深員工開發</li>
+							<li>適合預算較高</li>
+							<li>長期發展事業的公司</li>						
 						</ul>
 					</div>
-					<div class="chartRadarDiv" >
-						<canvas id="chartRadar"></canvas>
+					<div class="chartRadarDiv3" >
+						<canvas id="chartRadar3"></canvas>
 					</div>
 					<div class="service_price_primary">
 						<span class="service_per_month">
 							費用 : 
 						</span>
 						<span class="service_price">			
-							$5,000 - $15,000
+							$150,000 Up
 						</span>
 
 					</div>
@@ -174,7 +177,7 @@
 
     	console.log("---------Rader Data--------");
     	var graphData =new Array();
-    	graphData.push(90);
+    	graphData.push(60);
     	graphData.push(40);
     	graphData.push(40);
 	// graphData.push(20);
@@ -206,4 +209,147 @@
 		options: chartRadarOptions
 	});
 </script>
+
+<script>	
+	//定義變數
+	var chartRadarDOM2;
+	var chartRadarData2;
+	var chartRadarOptions2;
+	
+    	//載入雷達圖
+    	Chart.defaults.global.legend.display = false;
+    	Chart.defaults.global.defaultFontColor =' #908E65';
+    	chartRadarDOM2 = document.getElementById("chartRadar2");
+    	chartRadarData2;
+    	chartRadarOptions2 = 
+    	{
+    		scale: 
+    		{
+    			ticks: 
+    			{
+    				fontSize: 1,
+    				beginAtZero: true,
+    				maxTicksLimit: 5,
+    				min:0,
+    				max:100
+    			},
+    			pointLabels: 
+    			{
+    				fontSize: 12,
+    				color: '#F6490D'
+    			},
+    			gridLines: 
+    			{
+    				color: '#FFD768'
+    			}
+    		}
+    	};
+
+    	console.log("---------Rader Data--------");
+    	var graphData2 =new Array();
+    	graphData2.push(75);
+    	graphData2.push(60);
+    	graphData2.push(80);
+	// graphData.push(20);
+	// graphData.push(97);
+	
+	
+	console.log("--------Rader Create-------------");
+	console.log(graphData2);
+
+	//CreateData
+	chartRadarData2 = {
+		labels: ['Cost-effectiveness', 'Time benefit', 'Experience'],
+		datasets: [{
+			label: "Skill Level",
+			backgroundColor: "#2f5c6f",
+			borderColor: "rgba(63,63,74,.8)",
+			pointBackgroundColor: "rgba(63,63,74,1)",
+			pointBorderColor: "rgba(0,0,0,0)",
+			pointHoverBackgroundColor: "#fff",
+			pointHoverBorderColor: "rgba(0,0,0,0.3)",
+			pointBorderWidth: 5,
+			data: graphData2}]
+		};
+		
+	//Draw
+	var chartRadar2 = new Chart(chartRadarDOM2, {
+		type: 'radar',
+		data: chartRadarData2,
+		options: chartRadarOptions2
+	});
+</script>
+
+
+<script>	
+	//定義變數
+	var chartRadarDOM3;
+	var chartRadarData3;
+	var chartRadarOptions3;
+	
+    	//載入雷達圖
+    	Chart.defaults.global.legend.display = false;
+    	Chart.defaults.global.defaultFontColor =' #908E65';
+    	chartRadarDOM3 = document.getElementById("chartRadar3");
+    	chartRadarData3;
+    	chartRadarOptions3 = 
+    	{
+    		scale: 
+    		{
+    			ticks: 
+    			{
+    				fontSize: 1,
+    				beginAtZero: true,
+    				maxTicksLimit: 5,
+    				min:0,
+    				max:100
+    			},
+    			pointLabels: 
+    			{
+    				fontSize: 12,
+    				color: '#F6490D'
+    			},
+    			gridLines: 
+    			{
+    				color: '#FFD768'
+    			}
+    		}
+    	};
+
+    	console.log("---------Rader Data--------");
+    	var graphData3 =new Array();
+    	graphData3.push(100);
+    	graphData3.push(80);
+    	graphData3.push(90);
+	// graphData.push(20);
+	// graphData.push(97);
+	
+	
+	console.log("--------Rader Create-------------");
+	console.log(graphData3);
+
+	//CreateData
+	chartRadarData3 = {
+		labels: ['Cost-effectiveness', 'Time benefit', 'Experience'],
+		datasets: [{
+			label: "Skill Level",
+			backgroundColor: "#2f5c6f",
+			borderColor: "rgba(63,63,74,.8)",
+			pointBackgroundColor: "rgba(63,63,74,1)",
+			pointBorderColor: "rgba(0,0,0,0)",
+			pointHoverBackgroundColor: "#fff",
+			pointHoverBorderColor: "rgba(0,0,0,0.3)",
+			pointBorderWidth: 5,
+			data: graphData3}]
+		};
+		
+	//Draw
+	var chartRadar3 = new Chart(chartRadarDOM3, {
+		type: 'radar',
+		data: chartRadarData3,
+		options: chartRadarOptions3
+	});
+</script>
+
+
 @endsection
